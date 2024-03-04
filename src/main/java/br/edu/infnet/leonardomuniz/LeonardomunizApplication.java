6b5237a673f7f5afd8684aa7ded0cca139e555bf
Leonardo Muniz <131314978+leonardomunijz@users.noreply.github.com>
2024-03-04 13:12:54 -0300

    Add files via upload
    Commit inicial. uploading dos arquivos.

---
+++ src/main/java/br/edu/infnet/leonardomuniz/LeonardomunizApplication.java
@@ -0,0 +1,14 @@
+package br.edu.infnet.leonardomuniz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeonardomunizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeonardomunizApplication.class, args);
+
+	}
+
+}
